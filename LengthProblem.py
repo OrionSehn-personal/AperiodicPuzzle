8d5d59bcd26c5180ee5c4710ead588a3d6932189
OrionSehn <orion.sehn@gmail.com>
2022-03-12 14:18:06 -0700

    Completed length problem

--- LengthProblem.py
+++ LengthProblem.py
@@ -2,7 +2,19 @@ from turtle import distance
 import pandas as pd
 from random import uniform
 
-def spaced_points(x0, x1, epsilon):
+def spaced_x_vals(x0, x1, epsilon, paramList=[]):
+    '''
+    spaced generation for x values
+    '''
+    distance_series = spaced_distances(x0, x1, epsilon, paramList=[])
+    point_list = []
+    for i in range (len(distance_series)):
+        point_list.append(distance_series.head(i).sum())
+    
+    point_list.append(x1)
+    return point_list
+
+def spaced_distances(x0, x1, epsilon, paramList=[]):
     '''
     Returns a set of randomly distrubuted points such that no two points 
     are not within some minimum distance epsilon from each other.
@@ -18,14 +30,42 @@ def spaced_points(x0, x1, epsilon):
     distance_series = distance_series * (delta/t)
     return distance_series
 
-distances = spaced_points(0, 10, 2)
-point_list = []
-for i in range (len(distances)):
-    point_list.append(distances.head(i).sum())
-point_list.append(10)
-print(len(point_list))
-for i in range(len(point_list)-1):
-    print(f"this far apart: {point_list[i+1] - point_list[i]}")
+def spaced_y_vals(y0, y1, epsilon, paramList=[]):
+    points = spaced_distances(y0, y1, epsilon, paramList=[])
+    points = list(points)
+    for i in range(len(points)):
+        if i >= 5:
+            points[i] = -points[i]
+    points[1] = -points[1]
+    points[-2] = -points[-2]
+    distance_series = pd.Series(points)
+    point_list = []
+    for i in range (len(distance_series)):
+        point_list.append(distance_series.head(i).sum())
+    point_list.append(y0)
+    return point_list
 
-    
 
+def test1():
+    print("get x points")
+    point_list = spaced_x_vals(0, 10, 0.2)
+    print(point_list)
+    print(f"length: {len(point_list)}")
+    for i in range(len(point_list)-1):
+        print(f"this far apart: {point_list[i+1] - point_list[i]}")
+
+def test2():
+    print("get y points")
+    point_list = spaced_y_vals(0, 10, 0.2)
+    print(point_list)
+    print(f"length: {len(point_list)}")
+    for i in range(len(point_list)-1):
+        print(f"this far apart: {point_list[i+1] - point_list[i]}")
+
+def test3():
+    print("test spaced distances")
+    print(spaced_distances(0, 10, 0.2))
+if __name__ == "__main__":
+    test1()
+    test2()
+    test3()
